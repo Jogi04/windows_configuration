f4e6b95ef0c6a8604b1f88d357ec05808e5175e4
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2021-11-28 20:47:47 +0100

    Update remove_bloatware.ps1

--- remove_bloatware.ps1
+++ remove_bloatware.ps1
@@ -147,23 +147,5 @@ foreach ($app in $apps) {
     }
 }
 
-function Pin-App {param(
-        [string]$appname,
-        [switch]$unpin
-    )
-    try{
-        if ($unpin.IsPresent){
-            ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Von "Start" lösen|Unpin from Start'} | %{$_.DoIt()}
-            return "App '$appname' unpinned from Start"
-        }else{
-            ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'An "Start" anheften|Pin to Start'} | %{$_.DoIt()}
-            return "App '$appname' pinned to Start"
-        }
-    }catch{
-        Write-Error "Error Pinning/Unpinning App! (App-Name correct?)"
-    }
-}
-
-Pin-App "*spotify*" -unpin
 
 Write-Host `n "Done!" `n
